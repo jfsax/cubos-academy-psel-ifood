dfbef1aac27835a06db41355ffe8f2b2491e3c82
Jessica Xavier <jessicafsax@gmail.com>
2021-06-08 18:12:14 -0300

    Determina se num dado ano haverá jogos olímpicos, copa do mundo ou não

---
+++ copa-jogos-meh.js
@@ -0,0 +1,12 @@
+function solucao(ano) {
+	//seu codigo aqui
+    const resto = ano % 4;
+    
+    if (resto == 2){
+        console.log('COPA');
+    } else if (resto == 0){
+        console.log('JOGOS')
+    } else {
+        console.log('MEH');
+    }
+}
